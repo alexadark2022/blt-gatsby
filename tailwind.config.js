fa6688bd0db5f8c57a989a638270c0d694e2e590
Alexandra Spalato <alexaspalato@gmail.com>
2021-08-28 11:05:46 +0200

    Revert "tailwind and fonts"
    This reverts commit 12f5eb3519a340d5c48e9826dcb2e0bdc3293d37.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,155 +1,76 @@
+const colors = require("tailwindcss/colors")
 const rem = (px) => px / 16 + "rem"
-const defaultTheme = require("tailwindcss/defaultTheme")
 
 //palette
-const darkBlue = "#26496c",
-  lightBlue = "#a9e8ff",
-  veryLightBlue = "#c0eeff",
-  blue1 = "#eaecf2",
-  blueLink = "#3a8de1",
-  gold = "#d3b27d",
-  lightGold = "#d2be9b",
-  veryLightGold = "#f8f3ec",
-  black = "#000000",
-  grey5 = "#494949",
-  grey4 = "#676767",
-  grey3 = "#ababab",
-  grey2 = "#e1e1e1",
-  grey1 = "#f8f5f5",
-  white = "#fff",
-  orange = "#fda658"
+const cyan='#08fcfe',
+darkCyan="#417373",
+magenta="#f0a0ee",
+lightMagenta="#fdf3fd"
+green="#8df286",
+darkGreen="#445d42",
+lightGreen="#f0fdef"
+yellow="#feffa2",
+lightYellow="#ffffed"
+red="#fd9b9c",
+darkRed="#895354",
+black="#121211",
+white="#fff",
+blueWeb="#0000FF"
 
 module.exports = {
-  purge: ["./components/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}"],
-  mode: "jit",
-  darkMode: "class", // or 'media' or 'class'
-  theme: {
-    // ...defaultTheme,
+  purge: ["./src/**/*.{js,jsx,ts,tsx}"],
 
+  darkMode: "class",
+  mode: "jit",
+  theme: {
     extend: {
-      debugScreens: {
-        position: ["top", "left"],
-        style: {
-          backgroundColor: "black",
-          color: "#fff",
-        },
-      },
-      container: {
-        center: true,
-      },
-      fontSize: {
-        "f-12": rem(12),
-        "f-14": rem(14),
-        "f-16": rem(16),
-        "f-18": rem(18),
-        "f-22": rem(22),
-        "f-24": rem(24),
-        "f-26": rem(26),
-        "f-28": rem(28),
-        "f-30": rem(30),
-        "f-36": rem(36),
-        "f-40": rem(40),
-        "f-60": rem(60),
-        "f-64": rem(64),
-      },
-      colors: {
-        darkBlue,
-        lightBlue,
-        veryLightBlue,
-        blue1,
-        blueLink,
-        gold,
-        lightGold,
-        veryLightGold,
-        black,
-        grey1,
-        grey2,
-        grey3,
-        grey4,
-        grey5,
-        white,
-        orange,
-        primary: darkBlue,
-        highlight: gold,
-        text: grey4,
-      },
-      spacing: {
-        base: rem(15),
-        base2: rem(30),
-        "50px": "50px",
-      },
-      transitionProperty: {
-        height: "height",
-      },
-      borderRadius: {
-        sm: "3px",
-      },
-      boxShadow: {
-        section: "0 0 34px 0 rgba(0, 0, 0, 0.1)",
-        input: "0 2px 44px 0 rgba(0, 0, 0, 0.08)",
-        listing: "0 2px 24px 0 rgba(0, 0, 0, 0.08)",
-      },
       maxWidth: {
-        big: "1400px",
-      },
-      fontFamily: {
-        sans: `"Proxima Nova", "Trebuchet MS", sans-serif`,
-        script: `Adinda Melia`,
-      },
-      borderWidth: {
-        3: "3px",
+        lg: rem(1024),
+        md: rem(768),
+        sm: rem(640),
+        xl: rem(1280),
       },
       fontWeight: {
         body: 400,
-        heading: 400,
-        semiBold: 600,
+        heading: "bold",
         bold: 700,
+      },
+      lineHeight: {
+        body: 1.8,
+        heading: 1.4,
+        loose: 2,
+      },
+      boxShadow: {
+        input: "currentcolor 0px 0px 0px inset",
+        inputFocus: "currentcolor 0px -3px 0px inset",
       },
       typography: {
         DEFAULT: {
           css: {
-            color: grey4,
-            a: {
-              color: blueLink,
-              textDecoration: "none",
-              "&:hover": {
-                textDecoration: "underline",
-              },
-            },
-            "h1,h2,h3,h4,h5,h6": {
-              color: "#494949",
-            },
-            h2: {
-              fontSize: "36px",
-              color: "#494949",
-            },
-            ol: {
-              counterReset: "item",
-              listTyleType: "none",
-
-              li: {
-                marginBottom: "18px",
-                paddingLeft: "50px",
-                display: "flex",
-
-                "&:before": {
-                  content: 'counter(item)" "',
-                  counterIncrement: "item",
-                  fontWeight: "bold",
-                  fontSize: "22px",
-                  display: "flex",
-                  width: "30px",
-                  height: "30px",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  border: "3px solid",
-                  borderColor: gold,
-                  top: "4px",
-                },
-              },
-            },
+            a: {},
           },
         },
+      },
+
+      colors: {
+        cyan,
+        darkCyan,
+        green,
+        magenta,
+        lightMagenta,
+        lightGreen,
+        lightYellow,
+        darkGreen,
+        yellow,
+        red,
+        darkRed,
+        black,
+        white,
+        blueWeb,
+        link:blueWeb,
+        text: black
+
+
       },
     },
   },
@@ -160,7 +81,6 @@ module.exports = {
     require("@tailwindcss/typography"),
     require("@tailwindcss/forms"),
     require("tailwindcss-debug-screens"),
-    require("@tailwindcss/line-clamp"),
   ],
   corePlugins: {
     preflight: true,
