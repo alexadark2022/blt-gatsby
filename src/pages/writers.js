0c186f3e0c73f8a143362036e29b4a576b371d86
Alexandra Spalato <alexaspalato@gmail.com>
2021-08-31 12:40:17 +0200

    writers

--- src/pages/writers.js
+++ src/pages/writers.js
@@ -1,5 +1,11 @@
 import React from "react";
-import { useStaticQuery, graphql, Link } from "gatsby";
+import { useStaticQuery, graphql } from "gatsby";
+import { Layout, Section } from "../components";
+import { Newsletter } from "../components/Newsletter";
+import clsx from "clsx";
+import { Listing } from "../components/layout/Listing";
+import { ListingCard } from "../components/layout/ListingCard";
+import PageLayout from "../components/layout/PageLayout";
 
 const GET_ALL_WRITERS = graphql`
   query MyQuery {
@@ -13,9 +19,41 @@ const GET_ALL_WRITERS = graphql`
 
 const WritersPage = () => {
   const data = useStaticQuery(GET_ALL_WRITERS);
+  let writers = data?.allWpWriter?.nodes;
   console.log("data", data);
-  console.log("writers");
-  return <div></div>;
+
+  return (
+    <Layout>
+      <PageLayout
+        title="Our writers"
+        smallMargin
+        sidebar={
+          <>
+            <Newsletter />
+          </>
+        }
+      >
+        <Section className={clsx("p-5 md:py-10 md:px-7 mb-base2")}>
+          {writers?.map((item) => {
+            return (
+              <>
+                <Listing
+                  key={item.id}
+                  item={item}
+                  noBl
+                  writer
+                  className="hidden md:block"
+                />
+                <div className="flex justify-center mb-8 md:hidden">
+                  <ListingCard key={item.id} item={item} writer noBl />
+                </div>
+              </>
+            );
+          })}
+        </Section>
+      </PageLayout>
+    </Layout>
+  );
 };
 
 export default WritersPage;
