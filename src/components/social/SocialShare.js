cbc175c0690088561fba5779f4305abc59f120f6
Alexandra Spalato <alexaspalato@gmail.com>
2021-09-03 17:18:59 +0200

    sticky social

--- src/components/social/SocialShare.js
+++ src/components/social/SocialShare.js
@@ -1,32 +1,32 @@
-import React from "react"
+import React from "react";
 import {
   FaFacebookF as Facebook,
   FaWhatsapp as Whatsapp,
   FaTwitter as Twitter,
   FaEnvelope as Email,
-} from "react-icons/fa"
+} from "react-icons/fa";
 import {
   EmailShareButton,
   FacebookShareButton,
   WhatsappShareButton,
   TwitterShareButton,
-} from "react-share"
+} from "react-share";
 
-import clsx from "clsx"
-import tw from "twin.macro"
-import styled from "@emotion/styled"
-import { Section, Typo } from ".."
+import clsx from "clsx";
+import tw from "twin.macro";
+import styled from "@emotion/styled";
+import { Section, Typo } from "..";
 
 const RoundIcon = styled.span(() => [
   tw`w-10 h-10`,
   tw`flex items-center justify-center`,
   tw`bg-lightBlue`,
   tw`rounded-full`,
-])
+]);
 
 const SocialShare = ({ url, className = null }) => {
   const iconStyle =
-    "text-[20px] text-darkBlue hover:text-gold transition duration-300"
+    "text-[20px] text-darkBlue hover:text-gold transition duration-300";
   return (
     <div className={clsx("flex justify-center space-x-7", className)}>
       <FacebookShareButton url={url}>
@@ -50,18 +50,21 @@ const SocialShare = ({ url, className = null }) => {
         </RoundIcon>
       </EmailShareButton>
     </div>
-  )
-}
+  );
+};
 
 const SidebarSocialShare = ({ url }) => {
   return (
-    <Section as="div" className="px-3 pt-3 pb-5 space-y-3">
-      <Typo as="h3" className="text-center">
+    <Section
+      as="div"
+      className="fixed bottom-0 z-50 w-full px-3 pt-3 pb-5 -ml-5 space-y-3 md:static md:-ml-0"
+    >
+      <Typo as="h3" className="hidden text-center md:block">
         Share this page
       </Typo>
       <SocialShare url={url} />
     </Section>
-  )
-}
+  );
+};
 
-export { SocialShare, SidebarSocialShare }
+export { SocialShare, SidebarSocialShare };
