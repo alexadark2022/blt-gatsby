5c3d5f3309a78879da5e89ee147d762d7c1cb16c
Dipankar Maikap <45673791+dipankarmaikap@users.noreply.github.com>
2021-09-16 20:20:28 +0530

    Search item design added

--- src/components/my-components/SearchHit.js
+++ src/components/my-components/SearchHit.js
@@ -2,50 +2,176 @@ import React from "react";
 import { connectInfiniteHits } from "react-instantsearch-dom";
 import { Link } from "gatsby";
 import clsx from "clsx";
+import { FaTrashAlt as Trash } from "react-icons/fa";
 import { Button } from "../ui-components/Button";
 const SearchHit = ({ hits, hasMore, view, refineNext }) => {
+  let listView = view || "list";
+  console.log({ listView, view });
   return (
     <>
       <div
-        className={clsx("listings listings p-5 border shadow-lg", {
-          "p-0 border-0 shadow-none": view === "grid",
+        className={clsx("listings listings p-5 sm:border sm:shadow-lg", {
+          "p-0 border-0 sm:border-0 shadow-none sm:shadow-none":
+            listView === "grid",
         })}
       >
         <div
           className={clsx(
             "other ",
             {
-              "grid grid-cols-1 md:grid-cols-2  lg:grid-cols-3 gap-x-4":
-                view === "grid",
+              "grid grid-cols-1 sm:grid-cols-2  xl:grid-cols-3 gap-x-4":
+                listView === "grid",
             },
             {
-              "grid  grid-cols-1 gap-x-4": view === "list",
+              "grid  grid-cols-1 gap-x-4": listView === "list",
             }
           )}
         >
           {hits.map((hit) => {
             return (
-              <div key={hit.objectID} className="shadow-listing p-2 pr-3 mb-5">
-                <div className="max-w-[470px] flex flex-col justify-between">
-                  <div>
-                    <Link className="hover:no-underline " to={hit?.uri ?? "/"}>
-                      <div
-                        dangerouslySetInnerHTML={{
-                          __html: hit.title,
-                        }}
-                        className="font-bold leading-none text-grey4 text-f-24"
-                      >
-                        {/* {hit.title} */}
+              <div
+                key={hit.objectID}
+                className={clsx(
+                  {
+                    "shadow-listing pb-4 mb-5 relative max-w-[249px] mx-auto ":
+                      listView === "grid",
+                  },
+                  {
+                    "shadow-listing pb-4 sm:pb-3 sm:p-2 mb-5 relative max-w-[249px]  mx-auto sm:max-w-none":
+                      listView === "list",
+                  }
+                )}
+              >
+                <div
+                  className={clsx(
+                    {
+                      "flex flex-col justify-between": listView === "grid",
+                    },
+                    {
+                      "flex flex-col sm:flex-row justify-between":
+                        listView === "list",
+                    }
+                  )}
+                >
+                  <div
+                    className={clsx(
+                      {
+                        "flex flex-col": listView === "grid",
+                      },
+                      {
+                        "flex flex-col sm:flex-row  sm:space-x-4":
+                          listView === "list",
+                      }
+                    )}
+                  >
+                    <div className="image">
+                      <div className="w-[249px] h-[166px]">
+                        <img
+                          src={hit?.featuredImage?.node?.sourceUrl ?? ""}
+                          alt=""
+                        />
                       </div>
-                    </Link>
-                    {hit.commonDataAttributes.textCountry}
-                    <div className="mt-4 mb-3 mr-2 leading-tight prose">
-                      <div
-                        dangerouslySetInnerHTML={{
-                          __html: hit.commonDataAttributes.standfirst,
-                        }}
-                      />
                     </div>
+                    <div
+                      className={clsx(
+                        {
+                          "rest p-4": listView === "grid",
+                        },
+                        {
+                          "rest p-4 sm:p-0": listView === "list",
+                        }
+                      )}
+                    >
+                      <div className="max-w-[470px] flex flex-col justify-between">
+                        <div>
+                          <Link
+                            className={clsx(
+                              {
+                                "hover:no-underline text-center":
+                                  listView === "grid",
+                              },
+                              {
+                                "hover:no-underline text-center sm:text-left":
+                                  listView === "list",
+                              }
+                            )}
+                            to={hit?.uri ?? "/"}
+                          >
+                            <div
+                              dangerouslySetInnerHTML={{
+                                __html: hit.title,
+                              }}
+                              className="font-bold leading-none text-grey4 text-f-24"
+                            >
+                              {/* {hit.title} */}
+                            </div>
+                          </Link>
+                          <div
+                            className={clsx(
+                              {
+                                "text-center": listView === "grid",
+                              },
+                              {
+                                "text-center sm:text-left": listView === "list",
+                              }
+                            )}
+                          >
+                            {hit.commonDataAttributes.textCountry}
+                          </div>
+                          <div className="mt-4 mb-3 mr-2 leading-tight prose">
+                            <div
+                              dangerouslySetInnerHTML={{
+                                __html: hit.commonDataAttributes.standfirst,
+                              }}
+                            />
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div
+                    className={clsx(
+                      {
+                        "action flex flex-col items-center justify-between":
+                          listView === "grid",
+                      },
+                      {
+                        "action flex flex-col items-center sm:items-end justify-between":
+                          listView === "list",
+                      }
+                    )}
+                  >
+                    <div
+                      className={clsx(
+                        {
+                          "absolute top-0 right-0 mr-4 mt-4":
+                            listView === "grid",
+                        },
+                        {
+                          "absolute top-0 right-0 mr-4 mt-4 sm:relative sm:mr-0 sm:mt-0":
+                            listView === "list",
+                        }
+                      )}
+                    >
+                      <Button
+                        type="button"
+                        secondary
+                        className={clsx(
+                          "w-10 h-10 !p-0 !bg-transparent cursor-pointer hover:!bg-lightBlue"
+                        )}
+                      >
+                        <Trash className="text-gold text-[20px]" />
+                      </Button>
+                    </div>
+                    <Link
+                      className="hover:no-underline"
+                      href={hit?.uri ?? "/"}
+                      passHref
+                    >
+                      <Button as="a" secondary className="!text-[11px]">
+                        Read review
+                      </Button>
+                    </Link>
                   </div>
                 </div>
               </div>
@@ -55,7 +181,7 @@ const SearchHit = ({ hits, hasMore, view, refineNext }) => {
       </div>
       <div
         className={clsx("flex justify-center", {
-          "mt-6": view === "list",
+          "mt-6": listView === "list",
         })}
       >
         <Button disabled={!hasMore} onClick={refineNext} className="h-10">
