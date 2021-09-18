6531f196194cb91d2c8b157bc06175047210edc0
Alexandra Spalato <alexaspalato@gmail.com>
2021-09-18 15:43:20 +0200

    loader

--- src/components/auth/AuthContent.tsx
+++ src/components/auth/AuthContent.tsx
@@ -1,29 +1,32 @@
-import * as React from "react"
-import { useEffect, ReactNode, useState } from "react"
-import { navigate } from "gatsby"
-import { useAuth } from "../../lib/hooks/useAuth"
-import { AuthModal } from "./AuthModal"
+import * as React from "react";
+import { useEffect, ReactNode, useState } from "react";
+import { navigate } from "gatsby";
+import { useAuth } from "../../lib/hooks/useAuth";
+import { AuthModal } from "./AuthModal";
+import Loader from "react-spinners/BeatLoader";
 
 export function AuthContent({ children }: { children: ReactNode }) {
-  const { loggedIn, loading } = useAuth()
-  const [isOpen, setIsOpen] = useState(false)
+  const { loggedIn, loading } = useAuth();
+  const [isOpen, setIsOpen] = useState(false);
 
   // Navigate unauthenticated users to Home page.
   useEffect(() => {
     if (!loading && !loggedIn) {
-      navigate("/")
-      setIsOpen(true)
+      navigate("/");
+      setIsOpen(true);
     }
-  }, [loggedIn, loading, navigate])
+  }, [loggedIn, loading, navigate]);
 
   if (loggedIn) {
-    return <>{children}</>
+    return <>{children}</>;
   }
 
   return (
     <>
-      <p>Loading...</p>
+      <div className="flex justify-center py-20">
+        <Loader size={20} color="#d3b27d" />
+      </div>
       <AuthModal isOpen={isOpen} setIsOpen={setIsOpen} warning={false} />
     </>
-  )
+  );
 }
