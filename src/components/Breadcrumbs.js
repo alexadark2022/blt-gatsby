c33ac86a9b5e34fca5be196216c3fbc90f300cf2
Alexandra Spalato <alexaspalato@gmail.com>
2021-10-11 10:21:07 +0200

    breadcrumbs

--- src/components/Breadcrumbs.js
+++ src/components/Breadcrumbs.js
@@ -1,21 +1,21 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from "react";
+import { Link } from "gatsby";
 
 export const Breadcrumbs = ({ terms = [] }) => {
   return (
-    <div className="flex container max-w-big 2xl:px-0 px-5  pt-3">
+    <div className="container flex flex-wrap px-5 pt-3 text-sm sm:text-base max-w-big 2xl:px-0">
       {terms?.map((term, index) => {
-        const { name, link } = term || {}
+        const { name, link } = term || {};
 
         return (
           <div className="uppercase">
             {link ? <Link to={link}>{name}</Link> : `${name}`}
             {index !== terms?.length - 1 && (
-              <span className="inline-block mx-3">{`>`}</span>
+              <span className="inline-block mx-2 sm:mx-3">{`>`}</span>
             )}
           </div>
-        )
+        );
       })}
     </div>
-  )
-}
+  );
+};
