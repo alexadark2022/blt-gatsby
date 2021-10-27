814bc2f2c0d274963d37391d7897f5b162f639e4
Alexandra Spalato <alexaspalato@gmail.com>
2021-10-27 11:14:51 +0000

    zindex  add to bl

--- src/components/layout/ListingCard.js
+++ src/components/layout/ListingCard.js
@@ -83,13 +83,13 @@ export const ListingCard = ({
               <AddToBlButton
                 remove
                 addToBl={removeFromBl}
-                className="absolute z-30 px-2 py-1 text-2xl text-white cursor-pointer top-2 right-2"
+                className="absolute z-10 px-2 py-1 text-2xl text-white cursor-pointer top-2 right-2"
               />
             ) : (
               <AddToBlButton
                 add
                 addToBl={addToBl}
-                className="absolute z-30 px-2 py-1 text-2xl text-white cursor-pointer top-2 right-2"
+                className="absolute z-10 px-2 py-1 text-2xl text-white cursor-pointer top-2 right-2"
               />
             )
           ) : (
