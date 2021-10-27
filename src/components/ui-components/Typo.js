ae9ea3ffcc06bcd5378d1c16a68c6e43931e6c14
Alexandra Spalato <alexaspalato@gmail.com>
2021-10-27 11:10:11 +0000

    travelquote mobile

--- src/components/ui-components/Typo.js
+++ src/components/ui-components/Typo.js
@@ -12,16 +12,16 @@ export const Typo = styled.h2(({ h1, h3, h4, light, intro, travelQuote }) => [
   light && tw`text-white`,
   intro && tw`text-f-24 sm:text-f-24 text-grey4`,
   travelQuote &&
-    tw`leading-relaxed text-center sm:leading-loose text-gold font-script text-f-22 sm:text-f-40`,
+    tw`leading-relaxed text-center sm:leading-loose text-gold font-script text-f-24 sm:text-f-40`,
 ]);
 
 export const TravelQuote = ({ author, children, className, ...props }) => {
   return (
-    <div className={clsx("container px-5 sm:my-10", className)} {...props}>
+    <div className={clsx("container px-5 my-5 sm:my-10", className)} {...props}>
       <Typo as="h2" travelQuote>
         {children}
       </Typo>
-      <div className="text-center text-gold font-light capitalize text-[30px] mt-10">
+      <div className="text-center text-gold font-light capitalize text-f-22 sm:text-[30px] mt-5">
         {author}
       </div>
     </div>
