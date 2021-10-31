78098cb7e59e459478bedfbeb26a82abff7fc81a
Dipankar Maikap <45673791+dipankarmaikap@users.noreply.github.com>
2021-11-01 01:32:01 +0530

    Filter null lat or lang from google map

--- src/components/maps/FormatMapsData.js
+++ src/components/maps/FormatMapsData.js
@@ -1,24 +1,34 @@
 export default function FormatMapsData(hits) {
   return (
-    hits?.map((hit) => {
-      const {
-        title,
-        uri,
-        id,
-        featuredImage,
-        customDataAttributes: { latitudeOfLocation1, longitudeOfLocation1 },
-      } = hit || {};
+    hits
+      ?.filter((hit) => {
+        const {
+          customDataAttributes: { latitudeOfLocation1, longitudeOfLocation1 },
+        } = hit || {};
+        if (latitudeOfLocation1 && longitudeOfLocation1) {
+          return true;
+        }
+        return false;
+      })
+      .map((hit) => {
+        const {
+          title,
+          uri,
+          id,
+          featuredImage,
+          customDataAttributes: { latitudeOfLocation1, longitudeOfLocation1 },
+        } = hit || {};
 
-      return {
-        position: {
-          lat: latitudeOfLocation1,
-          lng: longitudeOfLocation1,
-        },
-        id,
-        title,
-        image: featuredImage?.node?.sourceUrl,
-        uri: uri,
-      };
-    }) ?? null
+        return {
+          position: {
+            lat: latitudeOfLocation1,
+            lng: longitudeOfLocation1,
+          },
+          id,
+          title,
+          image: featuredImage?.node?.sourceUrl,
+          uri: uri,
+        };
+      }) ?? null
   );
 }
