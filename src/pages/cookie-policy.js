cd64e91436a75c3cf139f196e347e551ef640e04
Dipankar Maikap <45673791+dipankarmaikap@users.noreply.github.com>
2021-11-18 01:02:11 +0530

    cookie policy script added

---
+++ src/pages/cookie-policy.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function CookiePolicy() {
+  return (
+    <div>
+      <div id="cookiefirst-policy-page"></div>{" "}
+      <div>
+        This cookie policy has been created and updated by{" "}
+        <a href="https://cookiefirst.com">CookieFirst.com</a>.
+      </div>
+    </div>
+  );
+}
